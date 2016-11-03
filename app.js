850a916fb3fc5beaff0845f954aff32f7a59e9e9
Kirill Leskin <olivez1197@gmail.com>
2016-11-03 13:28:21 +0300

    try create OOP in app

--- app.js
+++ app.js
@@ -14,99 +14,6 @@ console.log("Hello from server!");
 var SOCKET_LIST = {};
 
 /*
-function extend(Child, Parent) {
-   var F = function() { };
-   F.prototype = Parent.prototype;
-   Child.prototype = new F();
-   Child.prototype.constructor = Child;
-   Child.superclass = Parent.prototype;
-}
-
-var Entity = function () {
-   this.id = id;
-   this.x = 0;
-   this.y = 0;
-   this.spdX = 0;
-   this.spdY = 0;
-
-   this.getId = function () {
-      return this.id;
-   };
-   this.setId = function (id) {
-      this.id = id;
-   };
-   this.getX = function () {
-      return this.x;
-   };
-   this.setX = function (x) {
-      this.x = x;
-   };
-   this.getSpdX = function () {
-      return this.spdX;
-   };
-   this.setSpdX = function (spdX) {
-      this.spdX = spdX;
-   };
-   this.getY = function () {
-      return this.y;
-   };
-   this.setY = function (y) {
-      this.y = y;
-   };
-   this.getSpdY = function () {
-      return this.spdY;
-   };
-   this.setSpdY = function (spdY) {
-      this.spdY = spdY;
-   };
-
-   this.update = function () {
-      this.updatePosition();
-   };
-
-   this.updatePosition = function () {
-      this.x += this.spdX;
-      this.y += this.spdY;
-   };
-};
-
-var Player = function (id) {
-   this.superclass.constructor.apply(this, arguments);
-   this.number = ""+Math.floor(10*Math.random());
-   this.pressingLeft = false;
-   this.pressingRight = false;
-   this.pressingUp = false;
-   this.pressingDown = false;
-   this.spd = 1;
-   this.maxSpd = 8;
-
-   this.update = function () {
-     this.updateSpd();
-      this.superclass.update();
-   };
-
-   this.updateSpd = function () {
-      if(this.pressingRight){
-         this.superclass.setSpdX(this.maxSpd * this.spd);
-      }
-      else if(this.pressingLeft){
-         this.superclass.setSpdX(-this.maxSpd * this.spd);
-      }
-      else if(this.pressingDown){
-         this.superclass.setSpdY(this.maxSpd * this.spd);
-      }
-      else if(this.pressingUp){
-         this.superclass.setSpdY(-this.maxSpd * this.spd);
-      }
-      else
-         this.spd = 0;
-   };
-
-   Player.list[id] = this;
-   return this;
-};
-extend(Player, Entity);
-*/
 
 var Entity  = function () {
    var self = {
@@ -193,6 +100,106 @@ Player.update = function () {
 
    return pack;
 };
+*/
+
+function extend(Child, Parent) {
+   var Temp = function(){};
+   Temp.prototype = Parent.prototype;
+   Child.prototype = new Temp();
+   Child.prototype.constructor = Child;
+}
+
+function Entity(id) {
+   this.id = id;
+   this.X = 0;
+   this.Y = 0;
+   this.spdX = 0;
+   this.spdY = 0;
+}
+
+Entity.prototype = {
+   update: function() {
+      this.updatePosition();
+   },
+   updatePosition: function(){
+      this.X += this.spdX;
+      this.Y += this.spdY;
+   }
+};
+
+
+function Player(id){
+   Entity.prototype.id = id;
+   this.number = "" + Math.floor(10 * Math.random());
+   this.pressingRight = false;
+   this.pressingLeft = false;
+   this.pressingUp = false;
+   this.pressingDown = false;
+   this.spd = 1;
+   this.maxSpd = 10;
+
+   Player.list[id] = this;
+}
+
+Player.prototype = {
+   update: function () {
+      this.updateSpd();
+      Entity.prototype.update.apply(this);
+   },
+   updateSpd: function (){
+      if(this.pressingLeft)
+         this.spdX = -this.maxSpd*this.spd;
+      else if(this.pressingRight)
+         this.spdX = this.maxSpd*this.spd;
+      else
+         this.spdX = 0;
+
+      if(this.pressingUp)
+         this.spdY = -this.maxSpd*this.spd;
+      else if(this.pressingDown)
+         this.spdY = this.maxSpd*this.spd;
+      else
+         this.spdY = 0;
+   }
+};
+
+Player.list = {};
+
+extend(Player, Entity);
+
+
+function playerConnect(socket) {
+   var player = new Player(socket.id);
+
+   socket.on('keyPress', function (data) {
+      switch (data.inputId){
+         case 'left' : player.pressingLeft = data.state; break;
+         case 'right' : player.pressingRight = data.state; break;
+         case 'up' : player.pressingUp = data.state; break;
+         case 'down' : player.pressingDown = data.state; break;
+      }
+   });
+}
+
+function onPlayerDisconnect(socket) {
+   delete Player.list[socket.id];
+}
+
+function playerUpdate(){
+   var pack = [];
+   for(var i in Player.list){
+      var player = Player.list[i];
+      player.update();
+
+      pack.push({
+         x: player.X,
+         y: player.Y,
+         number: player.number
+      });
+   }
+
+   return pack;
+}
 
 
 var io = require('socket.io')(serv, {});
@@ -200,17 +207,17 @@ io.sockets.on('connection', function (socket) {
    socket.id = Math.random();
    SOCKET_LIST[socket.id] = socket;
 
-   Player.onConnect(socket);
+   playerConnect(socket);
 
    socket.on('disconnect', function () {
       delete SOCKET_LIST[socket.id];
-      Player.onDisconnect(socket);
+      onPlayerDisconnect(socket);
    });
 });
 
 
 setInterval(function () {
-   var pack = Player.update();
+   var pack = playerUpdate();
    for(var i in SOCKET_LIST){
       var socket = SOCKET_LIST[i];
       socket.emit('positions', pack);
